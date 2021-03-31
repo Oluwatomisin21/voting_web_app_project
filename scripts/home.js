d295b2d53dd171906eaff8e598567ddab1886cb5
callezenwaka <callisezenwaka@gmail.com>
2021-03-31 04:51:26 +0100

    Added validation to login form

--- scripts/home.js
+++ scripts/home.js
@@ -1,9 +1,24 @@
+const usernameValidation = document.querySelector('#username--validation').innerHTML = '';
+const passwordValidation = document.querySelector('#password--validation').innerHTML = '';
 const loginButton = document.querySelector('#login-button');
 loginButton.addEventListener('click', fadeOutEffect)
 
 function fadeOutEffect(event) {
     event.preventDefault();
+    // Shortcuts to DOM Elements.
     const username = document.querySelector('#username').value;
+    const password = document.querySelector('#password').value;
+    if (username === '' || password === '') {
+        // Shortcuts to DOM Elements.
+        document.querySelector('#username--validation').innerHTML = 'Username is required!';
+        document.querySelector('#password--validation').innerHTML = 'Password is required!';
+        // Clear error messages on the page
+        setTimeout(() => {
+            document.querySelector('#username--validation').innerHTML = '';
+            document.querySelector('#password--validation').innerHTML = '';
+        }, 5000);
+        return;
+    }
     document.querySelector('.account-title').innerHTML = "Welcome" + " " + username+"!\nProceeding to voting page . . .";
     document.querySelector('.account-wrapper').classList.add("form-success");
     var fadeTarget = document.querySelector('form');
