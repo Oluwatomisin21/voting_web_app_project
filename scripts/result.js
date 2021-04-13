946ea092e2eecc0c994d9e42701e831c9a76c52a
anonymouspro21 <petertomisin@gmail.com>
2021-04-13 13:59:37 +0100

    updated HTML aspect

--- scripts/result.js
+++ scripts/result.js
@@ -1,18 +1,40 @@
-'use strict';
+// 'use strict'; <
 
-// let results = [{
-//         "name": "Domene",
-//         "votes": 70,
-//     },
-//     {
-//         "name": "Chinobi",
-//         "votes": 69,
-//     },
+// }
+// // let results = [{
+// //         "name": "Domene",
+// //         "votes": 70,
+// //     },
+// //     {
+// //         "name": "Chinobi",
+// //         "votes": 69,
+// //     },
 
-// ]
+// // ]
 
-// retrieve user from local storage
-var user = JSON.parse(localStorage.getItem('user'));
-if (!user) {
-    window.location.href = "/templates/home.html";
-}
+// // retrieve user from local storage
+// var user = JSON.parse(localStorage.getItem('user'));
+// if (!user) {
+//     window.location.href = "/templates/home.html";
+// } { /* <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script> */ } <
+// script type = "text/javascript" >
+//     google.charts.load("current", {
+//         packages: ["corechart"]
+//     });
+// google.charts.setOnLoadCallback(drawChart);
+
+// function drawChart() {
+//     var data = google.visualization.arrayToDataTable([
+//         ['vote', 'candidate'],
+//         ['candidate 1', 21],
+//         ['candidate 2', 41],
+//     ]);
+
+//     var options = {
+//         title: 'Governorship Election result',
+//         is3D: true,
+//     };
+
+//     var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
+//     chart.draw(data, options);
+// } { /* </script> */ }
