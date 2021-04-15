d221b29cab7d666a0d4b4f5e55e8fb55082e35ab
anonymouspro21 <petertomisin@gmail.com>
2021-04-15 12:12:06 +0100

    updated JavaScript

--- scripts/result.js
+++ scripts/result.js
@@ -22,3 +22,30 @@ votingButtons.forEach((element, i) => {
     element.innerHTML = candidates[i].votes;
     // console.log(candidates[i].votes)
 });
+
+//converter function 
+function converter(array) {
+    var str = '';
+    var header = '';
+    array.forEach((element, i) => {
+        var line = '';
+        for (var index in element) {
+            // console.log(index);
+            if (i == 0) {
+                if (header != '') header += ','
+                header += index.toUpperCase();
+            }
+            if (line != '') line += ',';
+
+            if (element[index] == '') {
+                line += null;
+            } else {
+                line += element[index];
+            }
+        }
+        str += line + '\r\n';
+    })
+    var arr = `${header}\r\n${str}`;
+    return arr;
+    // console.log(arr);
+}
